4241b099643b56fe8a4e3b13501a6d78b9fc3c07
eeheg <sjh8924@naver.com>
2023-01-27 11:29:42 +0900

    class

--- src/Ch04/_04_SwitchCase.java
+++ src/Ch04/_04_SwitchCase.java
@@ -2,6 +2,15 @@ package Ch04;
 
 public class _04_SwitchCase {
     public static void main(String[] args) {
-        
+        String score = "A+";
+        if(score.equals("A+")) {
+            System.out.println("전액 장학금");
+        }
+        else if(score.equals("A0") || score.equals("B+")) {
+            System.out.println("반액 장학금");
+        }
+        else {
+            System.out.println("장학금 대상 아님");
+        }
     }
 }
